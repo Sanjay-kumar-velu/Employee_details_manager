8bb86feef4bcb0f74237b3b708483a7c601c8223
sanjaykumar <sanjaykumarvelumani123@gmail.com>
2025-10-14 19:23:44 +0530

    Kubernetes changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,3 +9,19 @@ export default defineConfig({
     }
   }
 })
+
+/*import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://a4fe8263910484b3b878495fea7b776e-398127749.ap-southeast2.elb.amazonaws.com:3030',
+        changeOrigin: true,
+      }
+    },
+    allowedHosts: ['.elb.amazonaws.com']
+  }
+})*/
